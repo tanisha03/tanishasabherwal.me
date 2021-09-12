a512f188cbe46eb727aee4db5cd5cb61200ceaaa
Tanisha <tanisha031199@gmail.com>
2021-09-12 17:12:57 +0530

    added files

--- pages/Layout.js
+++ pages/Layout.js
@@ -36,7 +36,7 @@ export default function Home(props) {
           <Link href="/" aria-label="Tanisha Sabherwal Logo">
             <a>
               <svg width="40" height="40" viewBox="0 0 281 219" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M78.5 211.5V40H18.5L7 5H254L243 40C220.333 36.1667 174.5 32.5 174.5 55.5C174.5 78.5 201.5 88.5 201.5 88.5C201.5 88.5 276 111.5 276 155.479C276 205.639 230.5 218.5 179.5 211.5" stroke="black" stroke-width="10"/>
+              <path d="M78.5 211.5V40H18.5L7 5H254L243 40C220.333 36.1667 174.5 32.5 174.5 55.5C174.5 78.5 201.5 88.5 201.5 88.5C201.5 88.5 276 111.5 276 155.479C276 205.639 230.5 218.5 179.5 211.5" stroke="black" strokeWidth="10"/>
               </svg>
             </a>
           </Link>
