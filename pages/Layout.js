8dd7b00f8f3316b57d71308413a81b8fa4e85b16
Tanisha <tanisha031199@gmail.com>
2021-09-12 17:05:56 +0530

    added files

--- pages/Layout.js
+++ pages/Layout.js
@@ -1,6 +1,26 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from "next/link";
+import {Icons} from '../assets/Icon';
+
+const LINKS=[
+  // {
+  //     icon:'email',
+  //     link:'mailto:tanisha031199@gmail.com'
+  // },
+  {
+      icon:'twitter',
+      link:'https://twitter.com/tanishaaa03'
+  },
+  {
+      icon:'github',
+      link:'https://github.com/tanisha03'
+  },
+  // {
+  //     icon:'medium',
+  //     link:'https://medium.com/@tanisha031199'
+  // }
+];
 
 export default function Home(props) {
   return (
@@ -9,7 +29,22 @@ export default function Home(props) {
       <Head>
         <title>{props.title}</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'></link>
       </Head>
+
+      <nav>
+          <Link href="/" aria-label="Tanisha Sabherwal Logo">
+            <a>
+              <svg width="40" height="40" viewBox="0 0 281 219" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M78.5 211.5V40H18.5L7 5H254L243 40C220.333 36.1667 174.5 32.5 174.5 55.5C174.5 78.5 201.5 88.5 201.5 88.5C201.5 88.5 276 111.5 276 155.479C276 205.639 230.5 218.5 179.5 211.5" stroke="black" stroke-width="10"/>
+              </svg>
+            </a>
+          </Link>
+          {/* <ul>
+            <li>Garden</li>
+            <li>Talks</li>
+          </ul> */}
+      </nav>
       
       <main className={styles.main}>
         {props.children}
@@ -17,15 +52,16 @@ export default function Home(props) {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+            <div>
+                {
+                   LINKS.map(link=>(
+                       <Link href={link.link} aria-label={link.icon} target="_blank" rel="noopener">
+                           <a>{Icons?.social[link.icon]}</a>
+                       </Link>
+                   )) 
+                } 
+            </div>
+            © TANISHA SABHERWAL {new Date().getFullYear()}
       </footer>
 
     </div>
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,21 @@ import Layout from './Layout';
 export default function Home() {
   return (
     <Layout title="Tanisha Sabherwal">
-      <p>Home</p>
+          <div>
+                <h1>Hi 👋 I am Tanisha !</h1>
+                <p>This site is currently WIP</p>
+                {/* {
+                    ABOUT_DATA.description.map(paragraph=>(
+                        <>
+                            <p>{paragraph}</p><br/>
+                        </>
+                    ))
+                }
+                <p>If you would like to know more about me, feel free to <a href="https://twitter.com/tanishaaa03">connect on twitter</a>.</p><br/>
+                <DownloadLink href={ABOUT_DATA.resume} download>
+                    Download Resume {Icons.download}
+                </DownloadLink> */}
+            </div>
     </Layout>
   )
 }
