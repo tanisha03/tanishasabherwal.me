e517ee13af574a703f18df90b0630a08e28fa2fd
Tanisha <tanisha031199@gmail.com>
2021-09-30 01:04:37 +0530

    added payments blog

---
+++ components/articlePage.js
@@ -0,0 +1,73 @@
+import { Fragment } from "react";
+import { getDatabase, getPage, getBlocks } from "../lib/notion";
+import Link from "next/link";
+import styles from "./post.module.css";
+import { renderBlock, Text } from '../util/gargenUtil';
+
+
+export default function Post({ page, blocks, databaseId }) {
+  if (!page || !blocks) {
+    return <div />;
+  }
+  return (
+    <Layout title="Post">
+      <article className={styles.container}>
+        <h1 className={styles.name}>
+          <Text text={page.properties.Name.title} />
+        </h1>
+        <section>
+          {blocks.map((block) => (
+            <Fragment key={block.id}>{renderBlock(block)}</Fragment>
+          ))}
+          <Link href="/garden">
+            <a className={styles.back}>← Go back</a>
+          </Link>
+        </section>
+      </article>
+    </Layout >
+  );
+}
+
+export const getStaticPaths = async () => {
+  const database = await getDatabase(databaseId);
+  return {
+    paths: database.map((page) => ({ params: { id: page.id } })),
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const { id } = context.params;
+  const page = await getPage(id);
+  const blocks = await getBlocks(id);
+
+  // Retrieve block children for nested blocks (one level deep), for example toggle blocks
+  // https://developers.notion.com/docs/working-with-page-content#reading-nested-blocks
+  const childBlocks = await Promise.all(
+    blocks
+      .filter((block) => block.has_children)
+      .map(async (block) => {
+        return {
+          id: block.id,
+          children: await getBlocks(block.id),
+        };
+      })
+  );
+  const blocksWithChildren = blocks.map((block) => {
+    // Add child blocks if the block should contain children but none exists
+    if (block.has_children && !block[block.type].children) {
+      block[block.type]["children"] = childBlocks.find(
+        (x) => x.id === block.id
+      )?.children;
+    }
+    return block;
+  });
+
+  return {
+    props: {
+      page,
+      blocks: blocksWithChildren,
+    },
+    revalidate: 1,
+  };
+};
---
+++ pages/fintech/index.js
@@ -0,0 +1,65 @@
+import Head from "next/head";
+import Link from "next/link";
+import { getDatabase } from "../../lib/notion";
+import { Text } from '../../util/gargenUtil';
+import styles from './index.module.css'
+import Layout from '../Layout';
+
+export const databaseId = process.env.NOTION_PAYMENTS_BLOG_ID;
+
+export default function Home({ posts }) {
+  return (
+    <Layout>
+
+      <main className={styles.container}>
+        <header className={styles.header}>
+          <h1>Demystifying India's Digital Payments & FinTech</h1>
+          <p>
+            This is an attempt to destructure the digital payments and fintech space and learn bit by bit. Everything about digital payments, products, processes and stakeholders that colloratively are working for a digitally enabled India.
+          </p>
+        </header>
+
+        <h2 className={styles.heading}>All Posts</h2>
+        <ol className={styles.posts}>
+          {posts.map((post) => {
+            const date = new Date(post.last_edited_time).toLocaleString(
+              "en-US",
+              {
+                month: "short",
+                day: "2-digit",
+                year: "numeric",
+              }
+            );
+            return (
+              <li key={post.id} className={styles.post}>
+                <h3 className={styles.postTitle}>
+                  <Link href={`/fintech/${post.id}`}>
+                    <a>
+                      <Text text={post.properties.Name.title} />
+                    </a>
+                  </Link>
+                </h3>
+
+                <p className={styles.postDescription}>{date}</p>
+                <Link href={`/fintech/${post.id}`}>
+                  <a> Read post →</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ol>
+      </main>
+    </Layout>
+  );
+}
+
+export const getStaticProps = async () => {
+  const database = await getDatabase(databaseId);
+
+  return {
+    props: {
+      posts: database,
+    },
+    revalidate: 1,
+  };
+};
